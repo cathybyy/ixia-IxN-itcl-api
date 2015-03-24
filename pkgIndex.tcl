8bd550fbc8306dbbdd4c4897bfa5e2e2b7613f89
Eric YU <leyu@ixiacom.com>
2015-03-24 23:09:14 +0800

    Create pkgIndex.tcl

---
+++ pkgIndex.tcl
@@ -0,0 +1,5 @@
+
+package ifneeded IxiaHL 2.4     [list source [file join $dir IxiaHL.tcl]]
+package ifneeded IxiaNet 4.60    [list source [file join $dir IxiaNet.tcl]]
+package ifneeded IxiaNetPython 1.0    [list source [file join $dir IxiaNetPython.tcl]]
+
