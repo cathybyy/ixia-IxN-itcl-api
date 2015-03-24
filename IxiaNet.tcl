64669320e778eafeefffb8e9fb7c2baaffaa9210
Eric YU <leyu@ixiacom.com>
2015-03-24 22:38:48 +0800

    Create IxiaNet.tcl

---
+++ IxiaNet.tcl
@@ -0,0 +1,683 @@
+
+# Copyright (c) Ixia technologies 2011-2012, Inc.
+
+set releaseVersion 4.62
+#===============================================================================
+# Change made
+# ==2011==
+# Version 1.0 
+#       1. Create
+# Version 1.1
+#       2. Release on June 12th
+# Version 1.2
+#		3. Release on June 14th 
+# Version 1.3
+# 		4. Release on June 15th
+# Version 1.4
+#		5. Release on June 28th
+# Version 1.5
+#		6. Release on June 30th
+#		6.1	Add-in IxOperate package for compatibility
+# Version 1.6
+#		7. Release on July 21st
+# Version 1.7
+#		8. Release on July 28th
+# Version 1.8
+#		9. Release on Aug 4th
+# Version 1.9
+#		10. Release on Aug 7th
+# Version 1.10
+#		11. Release on Aug 9th
+# Version 1.11
+#		12. Release on Aug 9th
+# Version 1.12
+#		13. Release on Aug 10th
+# Version 1.13
+#		14. Release on Aug 18th
+# Version 1.14
+#		15. Release on Aug 25th
+# Version 1.15
+#		16. Release on Aug 26th
+# version 1.16
+#		17. Release on Aug 30th
+# version 1.17
+#		18. Release on Sep 1st
+# Version 2.0
+#		19. Add Ospf/bgp interface
+#		19.1. Release on Sep 5th
+# Version 2.1
+#		20. Release on Sep 20th
+# Version 2.2
+#		21. Release on Oct 20th
+# Version 2.3
+#		22. Release on Oct 24th
+# Version 2.4
+#		23. Release on Nov 3rd
+# Version 2.5
+#		24. Multiuser login 
+#		24.1. Release on Nov 21st
+# Version 2.6
+#		25. Release on Nov 28th
+# Version 2.7
+#		26. Release on Nov 30th
+# Version 2.8
+#		27. Release on Dec 28th
+# Version 2.9
+#		28. Release on Dec 30th
+# ==2012==
+# Version 2.10
+#		29. Release on Jan 5th
+# Version 2.11
+#		30. Release on Jan 12th
+# Version 2.12
+#		31. Release on Jan 13th
+# Version 2.13
+#		32. Release on Jan 14th
+# Version 2.14
+#		33. Release on Jan 18th
+# Version 2.15
+#		34. Release on Jan 20th
+# Version 2.16
+#		35. Release on Feb 15th
+# Version 2.17
+#		36. Release on Feb 28th
+# Version 2.18
+#		37. Release on Mar 20th
+# Version 2.19
+#		38. Release on Mar 30th
+# Version 2.20
+#		39. Release on Apr 17th
+# Version 2.21
+#		40. Release on Apr 18th
+# Version 2.23
+# 		41. Release on May 5th
+# Version 2.24
+#		42. Release on May 22nd
+# Version 2.25
+#		43. Release on June 5th
+# Version 3.1
+#		44. Add Rfc2544/Trill/DCBX/FC/FCoE interface 
+#		44.1 Release on June 17th
+# Version 3.2
+#		45. Add DCBX Qaz TLV
+#		45.1 Release on June 26th
+# Version 3.3
+#		46. Release on June 28th
+# Version 3.5
+#		47. Release on July 5th
+# Version 3.6
+#		48. Release on July 10th
+# Version 3.7
+#		49. Release on July 11th
+# Version 3.8
+#		50. Release on July 17th
+# Version 3.9
+#		51. Release on July 18th
+# Version 3.10
+# 		52. Release on July 24th
+# Version 4.0
+#		53. Release on July 30th
+# Version 4.1
+#		54. Release on Aug 2nd
+# Version 4.2
+#       55. Release on Aug 17th
+# Version 4.3
+#		56. Release on Aug 17th
+# Version 4.4
+#		57. Release on Aug 22th
+# Version 4.5
+#		58. Release on Aug 24th
+# Version 4.6
+#		59. Release on Sep 10th
+# Version 4.7
+#		60. Release on Sep 12th
+#		60.1 include rfc2544
+# Version 4.8
+#		62. Release on Sep 24th
+# Version 4.9
+#		63. Release on Oct 8th
+#       63.1 Include Ospfv3/PIM/RIP
+# Version 4.10
+#		64. Release on Oct 15th
+# Version 4.11
+#		65. Release on Oct 22nd
+# Version 4.12
+#		66. Release on Nov 1st
+# Version 4.13
+#		67. Release on Nov 7th
+# Version 4.14
+#		68. Release on Nov 9th
+# Version 4.15
+#		69. Release on Nov 17th
+# Version 4.16
+#		70. Release on Nov 19th
+# Version 4.17
+#		71. Release on Nov 26th
+# Version 4.18
+#		72. Release on Nov 29th
+# Version 4.19
+#		73. Fix Tcl Proxy reconnection defect
+#		73.1 Release on Dec 12nd
+# Version 4.20
+#		74. Release on Dec 27th
+# ==2013==
+# Version 4.21
+#		75. Release on Jan 10th
+# Version 4.22
+#		76. Release on Jan 18th
+# Version 4.23
+#		77. Release on Feb 7th
+# Version 4.24
+#		78. Release on Feb 22th
+# Version 4.25
+#       79. Add log file ixlogfile
+#		79.1 Release on Mar 15th
+# Version 4.26
+#       80. Add force option in proc Login
+#       80.1 Release on Mar 29th
+# Version 4.27
+#       81. Change log file direction to c:/windows/temp/ixlogfile
+#       81.1 Release on Apr 12th
+# Version 4.28
+#       82. Release on Apr 19th
+# Version 4.29
+#       83. Release on May 8th
+# Version 4.30
+#       84. Release on Jun 21th
+# Version 4.31
+#       85. Release on Nov 7th
+# Version 4.31patch
+#		86. Release on Nov 24th
+# ==2014==
+# Version 4.32
+#       87. Add proc loadconfig, modify Login 
+#       87.1 source Ixia_NetDot1xRate.tcl
+#       87.2 Release on Mar 19th
+# Version 4.34
+#		88. Release on June 6th
+# Version 4.35
+#		89. Release on June 7th
+# Version 4.36
+#		90. Release on June 10th
+# Version 4.37
+#		91. Release on June 10th
+# Version 4.38
+#		92. Release on June 11th
+# Version 4.39
+#		93. Release on June 11th
+# Version 4.40
+#		94. Release on June 11th
+# Version 4.41
+#		95. Release on June 12th
+# Version 4.42
+#		96. Release on June 12th
+# Version 4.43
+#		97. Release on June 13th
+# Version 4.44
+#		98. Release on June 16th
+# Version 4.45
+# 		99. Release on June 24th
+# Version 4.46
+#		100. Release on June 30th
+# Version 4.47
+#		101. Release on July 1st
+# Version 4.48
+#		102. Release on July 3rd
+# Version 4.49
+#		103. Release on July 7th
+# Version 4.50
+#		104. Release on July 9th
+# Version 4.51
+#		105. Release on July 15th
+# Version 4.52
+#		106. Release on July 16th
+# Version 4.53
+#		107. Release on July 19th
+# Version 4.54
+#		108. Release on July 26th
+# Version 4.55
+#		109. Release on July 29th
+# Version 4.56
+#		110. Release on Aug 22nd
+# Version 4.57
+#		111. Release on Aug 31st
+# Version 4.58
+#		112. Release on Sep 1st
+# Version 4.59
+#		113. Release on Sep 7th
+# Version 4.60
+#		114. Release on Sep 16th
+# Version 4.61
+#		115. Release on Oct 11th
+# Version 4.62
+#		116. Add compatibility to tbc file source
+#		117. Release on Oct 16th
+
+proc GetEnvTcl { product } {
+   
+   set productKey     "HKEY_LOCAL_MACHINE\\SOFTWARE\\Ixia Communications\\$product"
+   set versionKey     [ registry keys $productKey ]
+   set latestKey      [ lindex $versionKey end ]
+
+   if { $latestKey == "Multiversion" } {
+      set latestKey   [ lindex $versionKey [ expr [ llength $versionKey ] - 2 ] ]
+   }
+   set installInfo    [ append productKey \\ $latestKey \\ InstallInfo ]            
+   return             [ registry get $installInfo  HOMEDIR ]
+
+}
+
+set portlist [list]
+set trafficlist [list]
+set portnamelist [list]
+set trafficnamelist [list]
+set tportlist [list]
+proc loadconfig { filename } {
+    global portlist
+    global trafficlist
+    global portnamelist
+    global trafficnamelist
+    global tportlist
+    puts "Loadconfig $filename"
+    ixNet exec loadConfig [ixNet readForm $filename]
+    set root [ixNet getRoot]
+    set portlist [ixNet getL $root vport]
+    foreach portobj $portlist {
+        lappend portnamelist [ixNet getA $portobj -name]
+    }
+    
+    set trafficlist [ixNet getL [ixNet getL $root traffic] trafficItem]
+    foreach trafficItemobj $trafficlist {
+        set itemlist [ixNet getL $trafficItemobj highLevelStream]
+        foreach trafficobj $itemlist {
+            lappend trafficnamelist [ixNet getA $trafficobj -name]
+            lappend tportlist [ixNet getA $trafficobj -txPortName]
+        }
+    }
+
+}
+
+proc Login { { location "localhost/8009"} { force 0 } { filename null } } {
+
+	global ixN_tcl_v
+	global loginInfo
+    
+    global portlist
+    global trafficlist
+    global portnamelist
+    global trafficnamelist
+    global tportlist
+    
+	global server
+	global serverPort
+	
+	set loginInfo $location
+puts "Login...$location"	
+	if { $location == "" } {
+		set port "localhost/8009"
+	} else {
+		set port $location
+	}
+
+	set portInfo [ split $port "/" ]
+	set server	 [ lindex $portInfo 0 ]
+	if { [ regexp {\d+\.\d+\.\d+\.\d+} $server ] || ( $server == "localhost" ) } {
+		set portInfo [ lreplace $portInfo 0 0 ]
+	} else {
+		set server localhost
+	}
+	if { [ llength $portInfo ] == 0 } {
+		set portInfo 8009
+	}
+    
+    set flag 0
+	foreach port $portInfo {
+		ixNet disconnect
+		ixNet connect $server -version $ixN_tcl_v -port $port
+		set root [ ixNet getRoot]
+		if { $force } {
+			puts "Login successfully on port $port."
+			#return	
+			set serverPort $port
+            set flag 1            
+		} else {
+			if { [ llength [ ixNet getL $root vport ] ] > 0 } {
+				puts "The connecting optional port $port is ocuppied, try next port..."
+				continue
+			} else {
+				puts "Login successfully on port $port."
+				#return
+				set serverPort $port
+                set flag 1
+			}
+		}
+        
+        if { $flag == 1 } {
+            if { $filename != "null" } {
+                loadconfig $filename
+				after 15000
+                
+                foreach pname $portnamelist pobj $portlist {
+                    Port $pname NULL NULL $pobj
+                }
+                
+                foreach tname $trafficnamelist tobj $trafficlist tport $portnamelist {
+                    Traffic $tname $tport $tobj
+                }
+				
+				return
+                
+            } else {
+                return
+            }
+        }
+	}
+	puts "Login failed on all port $portInfo."
+	return
+}
+
+proc GetAllPortObj {} {
+
+	set portObj [list]
+	set objList [ find objects ]
+	foreach obj $objList {
+		if { [ $obj isa Port ] } {
+			lappend portObj [ $obj cget -handle ]
+		}
+	}
+	return $portObj
+}
+
+set currDir [file dirname [info script]]
+puts "Package Directory $currDir"
+puts "load package Ixia_Util..."
+if { [ catch {
+	source [file join $currDir Ixia_Util.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_Util.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetObj..."
+if { [ catch {
+	source [file join $currDir Ixia_NetObj.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetObj.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetTester..."
+if { [ catch {
+	source [file join $currDir Ixia_NetTester.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetTester.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetPort..."
+if { [ catch {
+	source [file join $currDir Ixia_NetPort.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetPort.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetTraffic..."
+if { [ catch {
+	source [file join $currDir Ixia_NetTraffic.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetTraffic.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetDhcp..."
+if { [ catch {
+	source [file join $currDir Ixia_NetDhcp.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetDhcp.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetIgmp..."
+if { [ catch {
+	source [file join $currDir Ixia_NetIgmp.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetIgmp.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetCapture..."
+if { [ catch {
+	source [file join $currDir Ixia_NetCapture.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetCapture.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetCaptureFilter..."
+if { [ catch {
+	source [file join $currDir Ixia_NetCaptureFilter.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetCaptureFilter.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetOspf..."
+if { [ catch {
+	source [file join $currDir Ixia_NetOspf.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetOspf.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+}
+puts "load package Ixia_NetL3Vpn6Vpe..." 
+if { [ catch {
+	source [file join $currDir Ixia_NetL3Vpn6Vpe.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetL3Vpn6Vpe.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetLdp..."
+if { [ catch {
+	source [file join $currDir Ixia_NetLdp.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetLdp.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetIsis..."
+if { [ catch {
+	source [file join $currDir Ixia_NetIsis.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetIsis.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetTrill..."
+if { [ catch {
+	source [file join $currDir Ixia_NetTrill.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetTrill.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetDcbx..."
+if { [ catch {
+	source [file join $currDir Ixia_NetDcbx.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetDcbx.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetFcoe..."
+if { [ catch {
+	source [file join $currDir Ixia_NetFcoe.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetFcoe.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetPPPoX..."
+if { [ catch {
+	source [file join $currDir Ixia_NetPPPoX.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetPPPoX.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetBgp..."
+if { [ catch {
+	source [file join $currDir Ixia_NetBgp.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetBgp.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetRfc2544..."
+if { [ catch {
+	source [file join $currDir Ixia_NetRFC2544.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetRFC2544.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetDot1xRate..."
+if { [ catch {
+	source [file join $currDir Ixia_NetDot1xRate.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetDot1xRate.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetRip..."
+if { [ catch {
+	source [file join $currDir Ixia_NetRip.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetRip.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetPim...."
+if { [ catch {
+	source [file join $currDir Ixia_NetPim.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetPim.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package Ixia_NetBfd...."
+if { [ catch {
+	source [file join $currDir Ixia_NetBfd.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir Ixia_NetBfd.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+puts "load package IxOperate..."
+if { [ catch {
+	source [file join $currDir IxOperate.tcl]
+} err ] } {
+	if { [ catch {
+			source [file join $currDir IxOperate.tbc]
+	} tbcErr ] } {
+		puts "load package fail...$err $tbcErr"
+	}
+} 
+
+set errNumber(1)    "Bad argument value or out of range..."
+set errNumber(2)    "Madatory argument missed..."
+set errNumber(3)    "Unsupported parameter..."
+set errNumber(4)    "Confilct argument..."
+puts "set error message list..."
+
+set ixN_tcl_v "6.0"
+puts "connect to ixNetwork Tcl Server version $ixN_tcl_v"
+if { $::tcl_platform(platform) == "windows" } {
+	puts "windows platform..."
+	package require registry
+
+    if { [ catch {
+	    lappend auto_path  "[ GetEnvTcl IxNetwork ]/TclScripts/lib/IxTclNetwork"
+    } err ] } {
+		lappend auto_path $currDir/IxNetwork
+        puts "Failed to invoke IxNetwork environment...$err"
+		puts "Try to load IxNetwork local lib..."
+	}
+
+puts "load package IxTclNetwork..."
+	package require IxTclNetwork
+	puts "load package IxTclHal..."	
+	catch {	
+		source [ GetEnvTcl IxOS ]/TclScripts/bin/ixiawish.tcl
+	}
+	catch {package require IxTclHal}
+}
+
+package provide IxiaNet $releaseVersion
+puts "package require success on version $releaseVersion"
+
+# catch { console hide }
+
+rename ixNet IxNet
+proc ixNet { args } {
+	DeputsCMD "ixNet $args"
+	eval IxNet $args
+}
+
+if { [file exist "c:/windows/temp/ixlogfile"] } {
+} else {
+    file mkdir "c:/windows/temp/ixlogfile"
+}
+set timeVal  [ clock format [ clock seconds ] -format %Y%m%d_%H_%M ]
+set clickVal [ clock clicks ]
+set logfile_name "c:/windows/temp/ixlogfile/$timeVal.txt"
+
+IxDebugOn
+IxDebugCmdOn
